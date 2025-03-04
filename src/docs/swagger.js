a48d92d86382992672b3d20727e33949f58bb357
levionthemic <liem.levicoding834124@gmail.com>
2025-03-04 16:26:22 +0700

    Create Auth and Cart docs, ready for documenting APIs

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -4,8 +4,10 @@ import deepmerge from 'deepmerge'
 
 const baseDoc = JSON.parse(fs.readFileSync('./src/docs/swagger.json', 'utf8'))
 const productDoc = JSON.parse(fs.readFileSync('./src/docs/productDoc.json', 'utf8'))
+const authDoc = JSON.parse(fs.readFileSync('./src/docs/authDoc.json', 'utf8'))
+const cartDoc = JSON.parse(fs.readFileSync('./src/docs/cartDoc.json', 'utf8'))
 
-const swaggerDocument = deepmerge.all([baseDoc, productDoc])
+const swaggerDocument = deepmerge.all([baseDoc, productDoc, authDoc, cartDoc])
 
 const swaggerDocs = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
