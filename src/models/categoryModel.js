9b87b2d71e5e4b19ce44d5c845abc0882656b992
novinovinovinovi <thanhdungng04@gmail.com>
2025-03-19 15:47:09 +0700

    Done getCategoryList API

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 
@@ -10,13 +11,23 @@ const CATEGORY_COLLECTION_SCHEMA = Joi.object({
   avatar: Joi.string(),
   parentCategoryId: Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE).default(null),
   childrenCategoryId: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).optional(),
-  
+
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _deleted: Joi.boolean().default(false)
 })
 
+const getListCategories = async () => {
+  try {
+    const categoryList = await GET_DB().collection(CATEGORY_COLLECTION_NAME).find({ _deleted: false }).toArray()
+    return categoryList
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 export const categoryModel = {
   CATEGORY_COLLECTION_NAME,
-  CATEGORY_COLLECTION_SCHEMA
+  CATEGORY_COLLECTION_SCHEMA,
+  getListCategories
 }
